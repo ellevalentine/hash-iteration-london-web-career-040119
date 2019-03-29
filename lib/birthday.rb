3dbfb9c531523d0e5c86eb1b6de0bd788d072164
Elle Valentine <elle.valentine@hotmail.com>
2019-03-29 03:43:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,10 +15,9 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <=12
   print   "Happy Birthday #{kids_name}! You are now #{age} years old!" 
-  else ago>12
+  else age>12
     print "You are too old for this."
-end
+    
+     end
   end
-  
-  
 end
